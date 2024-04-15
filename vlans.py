4bd70c73cdcb11d3c52089016d64fe0bcbd04d18
Seb d'Argoeuves <sebastien.dargoeuves@ipfabric.io>
2024-04-16 00:42:36 +0100

    add README file

--- vlans.py
+++ vlans.py
@@ -1,34 +1,8 @@
 """
-# Description
-This script extracts the VLANs information from the SDN > ACI table, and use it to replace the vlanId in the Technology > VLANs table.
-It will generate a CSV file with the following columns:
-- hostname
-- siteName
-- vlanName
-- vlanId
-- status
-- stdStatus
-
-# How to use
-1. Set the environment variables (in a .env file or in the shell):
-- IPF_URL: IP Fabric URL
-- IPF_TOKEN: IP Fabric API token
-- IPF_VERIFY: (optional) Set to "True" to verify the SSL certificate
-- REPORT_OUTPUT: (optional) Path to the output CSV file (default: vlan_report.csv)
-
-2. Install the required packages
-pip install ipfabric
-
-3. Run the script
-python vlans.py
-
-# Example
-IPF_URL=https://ipfabric.local IPF_TOKEN=xxxx REPORT_OUTPUT=vlan_report.csv python vlans.py
-
-# Changelog
-- 2024-04-15: First version of the script
-
+This script extracts the VLANs information from the SDN > ACI table, 
+and use it to replace the vlanId in the Technology > VLANs table.
 """
+
 import csv
 import os
 
